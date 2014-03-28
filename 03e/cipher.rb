c40f0670f72fea0d2ca77997c95bac83528aa201
bahimi <dykester_03@hotmail.com>
2014-03-27 17:19:15 -0700

    added in evaluation for ranges and also join for final array

--- 03e/cipher.rb
+++ 03e/cipher.rb
@@ -1,10 +1,9 @@
 # Make a program that can encrypt texts with and alphabetical caesar cipher.  This cipher can ignore numbers, symbols, and whitespace.
 # For extra credit write a "decrypt" function to your program.
 
-
 #Get input from user
-string = arg[0]
-cipher = arg[1]
+string = ARGV[0]
+cipher = ARGV[1]
 
 #Separate into individual characters in an array
 by_char = string.split(//)
@@ -17,24 +16,25 @@ end
 
 by_char.map! do |d|
   if d == 32
-
   else
-    d += cipher.to_i
+    d += Integer(cipher)
+  end
+
+#If value is over 122, subtract 122 and add 96 to put it back in range, then convert back to characters using .chr
+# Switch case would probably work best here.  Evaluate ranges and select case to run for each.
+  if d > 122
+    d = (d - 122) + 96
+    d.chr
+  else
+  d.chr
   end
 end
 
-by_char.map! do |e|
-  e.chr
-end
-
-
-#Evaluate any in upcase range
-
+p by_char
 
 #For each value, add or subtract based on input by user (in range 1..25)
 
-#If value is over 122, subtract 122 and add 96 to put it back in range
-
-#Convert back to characters using .chr
-
 #Combine array into string using .join
+by_char = by_char.join('')
+
+p by_char
